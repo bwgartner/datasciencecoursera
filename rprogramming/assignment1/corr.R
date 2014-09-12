c919a80bebf8c09cc5f5a680997a03c9de1c1ec6
Bryan Gartner <bwgartner@gmail.com>
2014-09-11 18:26:52 -0700

    basic functionality implemented

--- rprogramming/assignment1/corr.R
+++ rprogramming/assignment1/corr.R
@@ -1,11 +1,37 @@
 corr <- function(directory, threshold = 0) {
         ## 'directory' is a character vector of length 1 indicating
         ## the location of the CSV files
+        if(file.exists(directory) && file.info(directory)[1,"isdir"]) {
+        } else {
+                stop("Cannot find data directory : ", directory)
+        }
+        #print("directory")
 
         ## 'threshold' is a numeric vector of length 1 indicating the
         ## number of completely observed observations (on all
         ## variables) required to compute the correlation between
         ## nitrate and sulfate; the default is 0
+        #print(threshold)
 
-        ## Return a numeric vector of correlations
+        # setup input data frame, data vector
+        temp <- data.frame()
+        result <- numeric()
+        options(digits=4)
+
+        # ready file handle list
+        fh_list <- list.files(directory, full.names=TRUE)
+        #print(length(fh_list))
+
+        #for( i in 1:2) {
+        for( i in 1:length(fh_list)) {
+                temp <- na.omit(read.csv(file=fh_list[i]))
+                #print(temp)
+                #print(nrow(temp))
+                if(nrow(temp) > threshold) {
+                        temp
+                        result <- c(result,cor(temp[["sulfate"]], temp[["nitrate"]]))
+                }
+        }
+        result
+
 }
